9cc2b9ba18265d5ad4648cdac70cbad27499282d
Ramon Smits <ramon.smits@gmail.com>
2020-05-27 01:14:51 +0200

    Support version 7.

--- src/NServiceBus.Compression/ConfigureExtensions.cs
+++ src/NServiceBus.Compression/ConfigureExtensions.cs
@@ -2,7 +2,7 @@
 {
     using System;
     using System.IO.Compression;
-    using NServiceBus.Configuration.AdvanceExtensibility;
+    using NServiceBus.Configuration.AdvancedExtensibility;
 
     public static partial class ConfigureExtensions
     {
